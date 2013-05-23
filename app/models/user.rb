d8da930736691cc9b6c03a95f222546bf30a5c9f
Andrew Stamm <andrewstamm@me.com>
2013-05-23 16:34:38 -0500

    added user validation method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  include BCrypt
+
   has_many :albums
   has_many :photos, :through => :albums
 
@@ -8,5 +10,21 @@ class User < ActiveRecord::Base
   validates :email, :uniqueness => true
   validates :email, :presence => true
   validates :password, :presence => true
+
+  before_save :encrypt
+
+  def self.authenticate(email, password)
+    user = User.find_by_email(email)
+    if user && Password.new(user.password) == password
+      true
+    else
+      false
+    end
+  end
+
+  def encrypt
+    to_hash = self.password
+    self.password = BCrypt::Password.create(to_hash)
+  end
 
 end
