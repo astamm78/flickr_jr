179bad9bb0f3a325ae1011cabe4516cbc21e3642
Andrew Stamm <andrewstamm@me.com>
2013-05-23 16:49:47 -0500

    added image uploader stuff

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,7 @@
 class Photo < ActiveRecord::Base
 
+  mount_uploader :image, AvatarUploader
+
   belongs_to :album
 
   validates :title, :presence => :true
--- db/migrate/20130523160344_create_photos.rb
+++ db/migrate/20130523160344_create_photos.rb
@@ -2,7 +2,7 @@ class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |p|
       p.string :title
-      p.string :url
+      p.string :image
       p.integer :album_id
     end
   end
