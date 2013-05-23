49af79a6f48bd5cbb53fb9b110c59bdc4738c0d4
Andrew Stamm <andrewstamm@me.com>
2013-05-23 16:13:29 -0500

    first commit

---
+++ db/migrate/20130523160338_create_albums.rb
@@ -0,0 +1,11 @@
+class CreateAlbums < ActiveRecord::Migration
+  def change
+    create_table :albums do |a|
+      a.string :name, :default => "Default"
+      a.integer :user_id
+      a.integer :photo_id
+
+      a.timestamps
+    end
+  end
+end
